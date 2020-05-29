051066d6c1f9785f389774398752695233b579fc
arleys.castro@gmail.com <arleys.castro@gmail.com>
2020-05-29 20:16:14 -0300

    Update OpenGlAdapter.java

--- Aula10/src/aula10/fja/edu/br/adapter/OpenGlAdapter.java
+++ Aula10/src/aula10/fja/edu/br/adapter/OpenGlAdapter.java
@@ -10,6 +10,7 @@ public class OpenGlAdapter extends OpenGLImagem implements ImagemTarget{
 
     @Override
     public void desenharImagem(int x, int y, int largura, int altura) {
+        //Lógica de programação de regras de negócio
         glDesenhaImagem(x-largura, y+altura);
     }
     
