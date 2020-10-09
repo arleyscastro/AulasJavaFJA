e664ef324711cf4f0c306172fb38c6fe5e05eb49
arleys.castro@gmail.com <arleys.castro@gmail.com>
2020-10-09 20:58:32 -0300

    CRUD
    Produto falta Deletar

---
+++ Praticas20202/Aula3/src/java/fja/edu/br/controladores/ProdutoController.java
@@ -0,0 +1,51 @@
+package fja.edu.br.controladores;
+
+import fja.edu.br.dominio.contratos.IProduto;
+import fja.edu.br.dominio.entidades.Produto;
+import fja.edu.br.infra.repositorio.ProdutoRepositorio;
+import java.util.List;
+
+
+public class ProdutoController {
+    private Produto produto;
+    private List<Produto> produtos = null;
+    
+    private IProduto prodRepositorio = new ProdutoRepositorio();
+
+
+    public ProdutoController() {
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public List<Produto> getProdutos() {
+        if(produtos == null){
+            produtos = prodRepositorio.getListaTodos();
+        }
+        return produtos;
+    }
+    
+    public String preparaInclusao(){
+        produto = new Produto();
+        return "vaiParaIncluir";
+    }
+    
+    public  String finalizaInclusao(){
+        prodRepositorio.incluir(produto);
+        produtos = null;
+        return "voltaParaListagem";
+    }
+    
+    public  String finalizaEdicao(){
+        prodRepositorio.alterar(produto);
+        produtos = null;
+        return "voltaParaListagem";
+    }
+    
+}
--- Praticas20202/Aula3/src/java/fja/edu/br/infra/repositorio/ProdutoRepositorio.java
+++ Praticas20202/Aula3/src/java/fja/edu/br/infra/repositorio/ProdutoRepositorio.java
@@ -4,7 +4,9 @@ import fja.edu.br.dominio.contratos.IProduto;
 import fja.edu.br.dominio.entidades.Produto;
 import fja.edu.br.infra.data.DaoUtil;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -41,22 +43,100 @@ public class ProdutoRepositorio extends DaoUtil implements IProduto{
 
     @Override
     public boolean alterar(Produto produto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "UPDATE produto SET nome = ?, preco = ?, validade = ?, descricao = ? WHERE idProduto = ?";
+        PreparedStatement ps;
+        int ret = 0;
+        try {
+            ps = getPreparedStatement(sql);
+            ps.setString(1, produto.getNome());
+            ps.setFloat(2, produto.getPreco());
+            ps.setDate(3, new java.sql.Date(produto.getValidade().getTime()) );
+            ps.setString(4, produto.getDescricao());
+            ps.setInt(5, produto.getIdproduto());
+            ret = ps.executeUpdate();
+            ps.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ProdutoRepositorio.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoRepositorio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return ret > 0;
     }
 
     @Override
     public boolean deletar(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "DELETE FROM produto WHERE idProduto = ?";
+        PreparedStatement ps;
+        int ret = 0;
+        try {
+            ps = getPreparedStatement(sql);
+            ps.setInt(1, id);
+            ret = ps.executeUpdate();
+            ps.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ProdutoRepositorio.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoRepositorio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return ret > 0;
     }
 
     @Override
     public Produto getConsultaPorId(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Produto prod = new Produto();
+        String sql = "SELECT idProduto, nome, preco, validade, descricao FROM produto WHERE idProduto = ?";
+        
+        PreparedStatement ps;
+        try {
+            ps = getPreparedStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while( rs.next() ){
+                prod = new Produto(rs.getInt("idProduto"),
+                                   rs.getString("nome"),
+                                   rs.getFloat("preco"),
+                                   rs.getDate("validade"),
+                                   rs.getString("descricao") ); 
+            }
+            rs.close();
+            ps.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ProdutoRepositorio.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoRepositorio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                
+        return prod;
     }
 
     @Override
     public List<Produto> getListaTodos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Produto> prodList = new LinkedList<Produto>();
+        String sql = "SELECT idProduto, nome, preco, validade, descricao FROM produto";
+        
+        PreparedStatement ps;
+        try {
+            ps = getPreparedStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while( rs.next() ){
+                prodList.add( new Produto(rs.getInt("idProduto"),
+                                   rs.getString("nome"),
+                                   rs.getFloat("preco"),
+                                   rs.getDate("validade"),
+                                   rs.getString("descricao") )
+                            ); 
+            }
+            rs.close();
+            ps.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ProdutoRepositorio.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoRepositorio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                
+        return prodList;
     }
     
 }
